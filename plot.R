52ed1ccca14af8fed05750bd12b35ac1efc23929
Wuyuan <wchen38@ucsc.edu>
2018-03-05 18:25:24 -0800

    found L1MKI and plotted with R

--- plot.R
+++ plot.R
@@ -3,10 +3,13 @@ myString <- "Hello, World! This is my plot!"
 print ( myString)
 pdf(file="./data/graphs/rplot.pdf")
 #read in csv file
-data <- read.csv(file="./data/csvs/t1_new.csv")
+data <- read.csv(file="./data/csvs/t1_new.csv", header=TRUE)
+#class(data)
+#head(data)
 #print("L1MPK: ", data[1])
 #plot it
-plot(data$L1MPKI ~ data$Timestamp, type="l", col="red", main="Benchmarks", xlab="timestamp", ylab="measurment")
+data$L1.dcache.load.misses <- data$L1.dcache.load.misses / (1000*data$instructions)
+plot(data$L1.dcache.load.misses ~ data$Timestamp, type="l", col="red", main="Benchmarks", xlab="timestamp", ylab="measurment")
 #plot(data$Frontend_Bound ~ data$Timestamp, type="o", col="red", main="Benchmarks", xlab="timestamp", ylab="measurment")
 #par(new=TRUE)
 #plot(data$Bad_Speculation ~ data$Timestamp, type="l", col="green", main="Benchmarks", xlab="timestamp", ylab="measurment")
--- toplev_plot.sh
+++ toplev_plot.sh
@@ -1,7 +1,10 @@
 input_csv=./data/csvs/t1.csv
 output_csv=./data/csvs/t1_new.csv
 
-toplev.py --all -I 15000 -x, -o $input_csv bash run_ocr.sh 1 8
+#toplev.py --all -I 15000 -x, -o $input_csv bash run_ocr.sh 1 8
+#interval-normalize.py $input_csv > $output_csv
+#Rscript plot.R
+perf stat -I 100 -e L1-dcache-load-misses,instructions -x, -o $input_csv bash run_ocr.sh 1 1
 interval-normalize.py $input_csv > $output_csv
 Rscript plot.R
 
