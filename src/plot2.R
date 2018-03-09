772a9647661b3d4562cc478a37a6c74d389d907e
Wuyuan <wchen38@ucsc.edu>
2018-03-08 21:55:18 -0800

    about to do 1T/64

--- src/plot2.R
+++ src/plot2.R
@@ -1,7 +1,7 @@
 # My first program in R Programming
 myString <- "Hello, World! This is my plot!"
 print ( myString)
-pdf(file="../data/graphs/t2/t2_1T_L1MKI.pdf")
+pdf(file="../data/graphs/t2/t2_1T64_L1MKI.pdf")
 #read in csv file
 data <- read.csv(file="../data/csvs/t2/t2_new.csv", header=TRUE)
 #class(data)
@@ -13,8 +13,8 @@ data$l2_rqsts.miss <- data$l2_rqsts.miss / (1000*data$instructions)
 data$branch.misses <- data$branch.misses / (1000*data$instructions)
 data$cycles <- data$instructions/data$cycles
 
-plot(data$L1.dcache.load.misses ~ data$Timestamp, type="l", col="red", main="Benchmarks", xlab="timestamp", ylab="measurment")
-plot(data$l2_rqsts.miss ~ data$Timestamp, type="l", col="red", main="thread1:1T inst L2MKI", xlab="timestamp", ylab="measurment")
-plot(data$branch.misses ~ data$Timestamp, type="l", col="red", main="thread1:1T inst branchMKI", xlab="timestamp", ylab="measurment")
-plot(data$cycles ~ data$Timestamp, type="l", col="red", main="thread1:1T inst IPC", xlab="timestamp", ylab="measurment")
+plot(data$L1.dcache.load.misses ~ data$Timestamp, type="l", col="red", main="1T/64", xlab="timestamp", ylab="measurment")
+plot(data$l2_rqsts.miss ~ data$Timestamp, type="l", col="red", main="thread1:1T/64 inst L2MKI", xlab="timestamp", ylab="measurment")
+plot(data$branch.misses ~ data$Timestamp, type="l", col="red", main="thread1:1T/64 inst branchMKI", xlab="timestamp", ylab="measurment")
+plot(data$cycles ~ data$Timestamp, type="l", col="red", main="thread1:1T/64 inst IPC", xlab="timestamp", ylab="measurment")
 #par(new=TRUE)
