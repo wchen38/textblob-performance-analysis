1a82f744cbc5b240c55a4ec9434976f055e454e5
Wuyuan <wchen38@ucsc.edu>
2018-03-08 21:21:23 -0800

    finish plot thread 1 with 1T inst

---
+++ src/plot4.R
@@ -0,0 +1,21 @@
+# My first program in R Programming
+myString <- "Hello, World! This is my plot!"
+print ( myString)
+pdf(file="../data/graphs/t4/t4_1T_L1MKI.pdf")
+#read in csv file
+data <- read.csv(file="../data/csvs/t4/t4_new.csv", header=TRUE)
+#class(data)
+#head(data)
+#print("L1MPK: ", data[1])
+#plot it
+data$L1.dcache.load.misses <- data$L1.dcache.load.misses / (1000*data$instructions)
+data$l2_rqsts.miss <- data$l2_rqsts.miss / (1000*data$instructions)
+data$branch.misses <- data$branch.misses / (1000*data$instructions)
+data$cycles <- data$instructions/data$cycles
+
+plot(data$L1.dcache.load.misses ~ data$Timestamp, type="l", col="red", main="Benchmarks", xlab="timestamp", ylab="measurment")
+plot(data$l2_rqsts.miss ~ data$Timestamp, type="l", col="red", main="thread1:1T inst L2MKI", xlab="timestamp", ylab="measurment")
+plot(data$branch.misses ~ data$Timestamp, type="l", col="red", main="thread1:1T inst branchMKI", xlab="timestamp", ylab="measurment")
+plot(data$cycles ~ data$Timestamp, type="l", col="red", main="thread1:1T inst IPC", xlab="timestamp", ylab="measurment")
+#par(new=TRUE)
+
---
+++ src/run_ocr.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+# number of cores over which the process will be parallelized
+num_thread=$1
+num_loop=$2
+t1_f1=../inputs/t1/1
+t2_f1=../inputs/t2/1
+t2_f2=../inputs/t2/2
+
+input_t1_csv=../data/csvs/t1.csv
+input_t2_csv=../data/csvs/t2/t2.csv
+input_t4_csv=../data/csvs/t4/t4.csv
+input_t8_csv=../data/csvs/t8/t8.csv
+
+output_t1_csv=../data/csvs/t1_new.csv
+output_t2_csv=../data/csvs/t2/t2_new.csv
+output_t4_csv=../data/csvs/t4/t4_new.csv
+output_t8_csv=../data/csvs/t8/t8_new.csv
+
+
+# runs OCR on the found TIFF files and converts them to text. Assumes English, but you can supply
+# extra arguments to tesseract
+#parallel --bar -j $num_cores ./text_blob.py
+     echo ------------------------using thread 1
+     perf stat -I 6000 -e L1-dcache-load-misses,instructions,l2_rqsts.miss,branch-misses,cycles -x, -o $input_t1_csv python text_blob.py $t1_f1 8
+     #perf stat -e L1-dcache-load-misses,instructions -x, -o $input_csv python text_blob.py $t1_f1 $num_loop
+     interval-normalize.py $input_t1_csv > $output_t1_csv
+     Rscript plot.R
+if [ 0 = 1 ]     
+then
+     echo -------------------------using thread 2
+     perf stat -I 3000 -e L1-dcache-load-misses,instructions,l2_rqsts.miss,branch-misses,cycles -x, -o $input_t2_csv parallel ::: "python text_blob.py $t1_f1 4" "python text_blob.py $t1_f1 4"
+     interval-normalize.py $input_t2_csv > $output_t2_csv
+     Rscript plot2.R    
+
+     echo -------------------------using thread 4
+     perf stat -I 1000 -e L1-dcache-load-misses,instructions,l2_rqsts.miss,branch-misses,cycles -x, -o $input_t4_csv parallel ::: "python text_blob.py $t1_f1 2" \
+     "python text_blob.py $t1_f1 2" "python text_blob.py $t1_f1 2"  "python text_blob.py $t1_f1 2"
+     interval-normalize.py $input_t4_csv > $output_t4_csv
+     Rscript plot4.R
+    
+
+     echo ------------------------ using thread 8
+     perf stat -I 1000 -e L1-dcache-load-misses,instructions,l2_rqsts.miss,branch-misses,cycles -x, -o $input_t8_csv parallel ::: "python text_blob.py $t1_f1 1" \
+     "python text_blob.py $t1_f1 1" "python text_blob.py $t1_f1 1"  "python text_blob.py $t1_f1 1" "python text_blob.py $t1_f1 1" \
+     "python text_blob.py $t1_f1 1" "python text_blob.py $t1_f1 1"  "python text_blob.py $t1_f1 1"
+     interval-normalize.py $input_t8_csv > $output_t8_csv
+     Rscript plot8.R
+     echo not using any threads
+fi
+
