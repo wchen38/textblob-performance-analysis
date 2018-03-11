0b177396ef64cd2bf5a425015e56512b22c3e2a0
Wuyuan <wchen38@ucsc.edu>
2018-03-11 04:45:45 -0700

    hi

--- docker/run.sh
+++ docker/run.sh
@@ -7,8 +7,8 @@ t1_1T16=textblob-performance-analysis/inputs/t1/small1
 t1_1T64=textblob-performance-analysis/inputs/t1/small1
 t1_1T256=textblob-performance-analysis/inputs/t1/small1
 
-input_t1_csv=//textblob-performance-analysis/data/csvs/t1.csv
-output_t1_csv=//textblob-performance-analysis/data/csvs/t1_new.csv
+input_t1_csv=../data/csvs/t1.csv
+output_t1_csv=../data/csvs/t1_new.csv
 
 
 if [[ $thread = "1" ]]
@@ -19,7 +19,7 @@ then
 	interval-normalize.py $input_t1_csv > $output_t1_csv
 	
 	perf stat -I 100 -e L1-dcache-load-misses,instructions,l2_rqsts.miss,branch-misses,cycles,LLC-load-misses -x, \
-        -o $input_t1_csv docker run -e thread=1 -e file="$t1_1T" test1
+        -o $input_t1_csv docker run -e thread=1 -e file="$t1_1T4" test1
 elif [[ $thread = "2" ]]
 then 
 	echo thread 2 in the run.sh file---------------------------------------------
