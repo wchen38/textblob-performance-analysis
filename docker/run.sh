7ebd72408e5a05848a9835b95f01425ac6527500
Wuyuan <wchen38@ucsc.edu>
2018-03-11 04:41:27 -0700

    testing more threads

--- docker/run.sh
+++ docker/run.sh
@@ -2,12 +2,32 @@
 thread=$1
 
 t1_1T=textblob-performance-analysis/inputs/t1/small1
+t1_1T4=textblob-performance-analysis/inputs/t1/small1
+t1_1T16=textblob-performance-analysis/inputs/t1/small1
+t1_1T64=textblob-performance-analysis/inputs/t1/small1
+t1_1T256=textblob-performance-analysis/inputs/t1/small1
 
-input_t1_csv=../data/csvs/t1.csv
+input_t1_csv=//textblob-performance-analysis/data/csvs/t1.csv
+output_t1_csv=//textblob-performance-analysis/data/csvs/t1_new.csv
 
 
 if [[ $thread = "1" ]]
 then
+	echo thread 1 in the run.sh file-------------------------------------------
 	perf stat -I 100 -e L1-dcache-load-misses,instructions,l2_rqsts.miss,branch-misses,cycles,LLC-load-misses -x, \
         -o $input_t1_csv docker run -e thread=1 -e file="$t1_1T" test1
+	interval-normalize.py $input_t1_csv > $output_t1_csv
+	
+	perf stat -I 100 -e L1-dcache-load-misses,instructions,l2_rqsts.miss,branch-misses,cycles,LLC-load-misses -x, \
+        -o $input_t1_csv docker run -e thread=1 -e file="$t1_1T" test1
+elif [[ $thread = "2" ]]
+then 
+	echo thread 2 in the run.sh file---------------------------------------------
+elif [[ $thread = "4" ]]
+then 
+	echo thread 4 in the run.sh file--------------------------------------------
+elif [[  $thread = "8" ]]
+	echo thread 8 in the run.sh file--------------------------------------------
+else 
+	echo invalid thread number
 fi
