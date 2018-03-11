925c25b1a5e908f5132a487e673721d1fdd9acbd
Wuyuan <wchen38@ucsc.edu>
2018-03-11 04:14:34 -0700

    5

--- docker/run.sh
+++ docker/run.sh
@@ -8,6 +8,5 @@ input_t1_csv=../data/csvs/t1.csv
 
 if [[ $thread = "1" ]]
 then
-	perf stat -I 50 -e L1-dcache-load-misses,instructions,l2_rqsts.miss,branch-misses,cycles,LLC-load-misses -x, \ 
-	-o $input_t1_csv docker run -e thread=1 -e file=$t1_1T test1
+	perf stat -I 100 -e L1-dcache-load-misses,instructions,l2_rqsts.miss,branch-misses,cycles,LLC-load-misses -x, -o $input_t1_csv docker run -e thread=1 -e file="$t1_1T" test1
 fi
