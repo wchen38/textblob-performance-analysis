0be2def28439bafbfe17e0bee05e7214f0c99ef3
Wuyuan <wchen38@ucsc.edu>
2018-03-11 01:48:02 -0800

    changes

--- docker/src/run_ocr.sh
+++ docker/src/run_ocr.sh
@@ -4,7 +4,7 @@
 # number of cores over which the process will be parallelized
 #$num_thread=$1
 #num_loop=$2
-t1_f1=./textblob-performance-analysis/inputs/t1/1T256
+t1_f1=textblob-performance-analysis/inputs/t1/1T256
 t2_f1=./textblob-performance-analysis/inputs/t2/1
 t2_f2=./textblob-performance-analysis/inputs/t2/2
 
@@ -22,9 +22,9 @@ output_t8_csv=../../data/csvs/t8/t8_new.csv
 # runs OCR on the found TIFF files and converts them to text. Assumes English, but you can supply
 # extra arguments to tesseract
 #parallel --bar -j $num_cores ./text_blob.py
-echo $thread
+echo $t1_f1
 pwd
-ls
+ls textblob-performance-analysis/inputs/t1
 if [ $thread = '1' ]
 then
      echo ------------------------using thread 1
